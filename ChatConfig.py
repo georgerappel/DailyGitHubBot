4804706e319ca247bf5d157d1b4b25a55f3b590c
George Rappel <george.concei@hotmail.com>
2019-05-24 15:14:20 -0300

    * Fix /config command to show config when still not set

--- ChatConfig.py
+++ ChatConfig.py
@@ -3,8 +3,16 @@ class ChatConfig:
 
     def __init__(self, chat_id="", username="", hour=0):
         self.chat_id = str(chat_id)
-        self.username = username
-        self.hour = hour
+
+        if username is None:
+            self.username = "(not set)"
+        else:
+            self.username = username
+
+        if hour is None:
+            self.hour = "(not set)"
+        else:
+            self.hour = hour
 
     def update(self, username, hour):
         if username is not None:
@@ -13,5 +21,4 @@ class ChatConfig:
             self.hour = hour
 
     def to_string(self):
-        return "Username: @" + self.username + "\n"\
-                + "Notification hour: " + str(self.hour)
+        return "Username: @" + self.username + "\n" + "Notification hour: " + str(self.hour)
